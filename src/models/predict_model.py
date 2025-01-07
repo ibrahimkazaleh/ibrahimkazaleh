6615f85d92a873ecae1427f113af344176641e2f
ibrahim kazaleh <ibrahimkazaleh@gmail.com>
2025-01-07 22:06:29 +0300

    tarn and test model on data and export best model

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,5 +1,5 @@
-import numpy as np
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from LearningAlgorithms import ClassificationAlgorithms
